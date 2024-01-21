c684629d782ac33ce6f9268e539bb595eac3a382
ae22-1944 <ae22-1944@unphu.edu.do>
2024-01-21 14:02:12 -0400

    Primer commit

---
+++ src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) {
+        String tipo = "Computadora";
+
+        final double TASA_DOLAR = 59.50;
+
+        boolean disponible = false;
+
+        final String MENSAJE_BIENVENIDA = "Bienvenido a este módulo";
+
+        int resultado = 4 * 9;
+
+        double monto = 775.49;
+
+        double montoConITBIS = monto + (monto * 0.18);
+
+        System.out.println(montoConITBIS);
+    }
+}
